f29d72424a8158c3a24c9490b4f0aebcecc8f232
cagkan caglar <cagkancaglar@hotmail.com>
2022-08-22 03:12:10 +0300

    create

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/main.scss'
+import App from './components/App'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Switch
+  } from "react-router-dom";
+
+
+const HomePage = () => {
+    <div>Home Page</div>
+};
+
+const ContactPage = () => {
+    <div>Contact Page</div>
+};
+
+const ProductsPage = () => {
+    <div>Products Page</div>
+};
+
+const NotFound = () => {
+    <div>404 Page</div>
+};
+
+
+const routes =  (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={ HomePage } />
+            <Route path="/contact" component={ ContactPage } />
+            <Route path="/products" component={ ProductsPage } />
+            <Route component={ NotFound } />
+        </Switch>
+    </BrowserRouter>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("node:path/win32");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    },
+    mode:'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                 test: /\.scss$/,
+                 use: ['style-loader','css-loader','sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            }
+        ]
+    }       
+}
