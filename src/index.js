4beea75ab0efdba5668ac9669aa3b219df69d57d
cagkan caglar <cagkancaglar@hotmail.com>
2022-08-23 15:59:21 +0300

    browser-router

--- src/index.js
+++ src/index.js
@@ -6,35 +6,73 @@ import {
     BrowserRouter,
     Routes,
     Route,
-    Switch
+    Switch,
+    Link
   } from "react-router-dom";
 
 
+const Nav = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link> |
+            <Link to="/contact">Contact</Link> |
+            <Link to="/products">Products</Link> |
+        </nav>
+    )
+};
+
+
 const HomePage = () => {
-   return <div>Home Page</div>
+    
+   return(
+    <>
+        <div>Home Page</div>
+    </>
+   ) 
 };
 
-const ContactPage = () => {
-    return <div>Contact Page</div>
-};
+ const ContactPage = () => {
+    
+    return(
+     <>
+         <div>Contact Page</div>
+     </>
+    ) 
+ };
 
-const ProductsPage = () => {
-    return <div>Products Page</div>
-};
+ const ProductsPage = () => {
+    
+    return(
+     <>
+         <div>Products Page</div>
+     </>
+    ) 
+ };
 
-const NotFound = () => {
-    return <div>404 Page</div>
-};
-
+ const NotFoundPage = () => {
+    
+    return(
+     <>
+         <div>NotFound Page</div>
+         <nav>
+             <Link to="/">Home</Link>
+             <Link to="/contact">Contact</Link>
+             <Link to="/products">Products</Link>
+         </nav>
+     </>
+    ) 
+ };
 
 const routes =  (
     <BrowserRouter>
+    
         <Switch>
             <Route exact path="/" component={ HomePage } />
             <Route path="/contact" component={ ContactPage } />
             <Route path="/products" component={ ProductsPage } />
-            <Route component={ NotFound } />
+            <Route component={ NotFoundPage } />
         </Switch>
+        <Nav />
     </BrowserRouter>
 );
 
