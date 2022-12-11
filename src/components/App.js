b1477ec4242021d545fb3b458c06b8c1a4fc53a9
Çağkan Çağlar <cagkancaglar@hotmail.com>
2022-12-11 16:10:25 +0300

    convert class to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter,
   Routes,
@@ -16,96 +16,88 @@ import About from "./About";
 // import AppRouter from "../routers/AppRouter";
 import UserDetails from "./UserDetails";
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.searchUsers = this.searchUsers.bind(this);
-    this.clearUsers = this.clearUsers.bind(this);
-    this.setAlert = this.setAlert.bind(this);
-    this.getUser = this.getUser.bind(this);
-    this.getUserRepos = this.getUserRepos.bind(this);
-    this.state = {
-      loading: false,
-      users: [],
-      user: {},
-      repos: [],
-      alert: null
-    }
-  }
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
+  const [alert, setAlert] = useState(null);
+  
 
-  searchUsers(keyword) {
-    this.setState({ loading: true });
+  const searchUsers = (keyword) => {
+    setLoading(true);
 
     setTimeout(() => {
       axios
         .get(`https://api.github.com/search/users?q=${keyword}`)
-        .then((res) =>
-          this.setState({ users: res.data.items, loading: false })
-        );
+        .then((res) =>{
+          setUsers(res.data.items)
+          setLoading(false)
+        });
     }, 1000);
   }
 
-  getUser(username) {
-    this.setState({loading: true,})
+  const getUser= (username) => {
+    setLoading(true);
     setTimeout(() => {
         axios
           .get(`https://api.github.com/users/${username}`)
-          .then((res) =>
-            this.setState({ user: res.data, loading: false })
-          );
+          .then((res) =>{
+            setUser(res.data)
+            setLoading(false)
+          });
       }, 1000);
   }
 
-  getUserRepos(username) {
-    this.setState({loading: true,})
+  const getUserRepos = (username) => {
+    setLoading(true);
+
     setTimeout(() => {
         axios
           .get(`https://api.github.com/users/${username}/repos`)
-          .then((res) =>
-            this.setState({ repos: res.data, loading: false })
-          );
+          .then((res) =>{
+            setRepos(res.data)
+            setLoading(false)
+          });
       }, 1000);
   }
 
-  clearUsers() {
-    this.setState({ users: [] });
+  const clearUsers = () => {
+    setUsers([]);
   }
 
-  setAlert(msg, type) {
-    this.setState({ alert: { msg, type } });
-
+  const showAlert = (msg, type) => {
+    setAlert({msg,type})
     setTimeout(() => {
       this.setState({ alert: null });
     }, 2000);
   }
-  render() {
     return (
       <BrowserRouter>
         <Navbar />
-        <Alert alert={this.state.alert} />
+        <Alert alert={alert} />
         <Switch>
           <Route exact
             path="/"
             render={(props) => (
               <>
                 <Search
-                  searchUsers={this.searchUsers}
-                  clearUsers={this.clearUsers}
-                  showClearButton={this.state.users.length > 0 ? true : false}
-                  setAlert={this.setAlert}
+                  searchUsers={searchUsers}
+                  clearUsers={clearUsers}
+                  showClearButton={users.length > 0 ? true : false}
+                  setAlert={showAlert}
                 />
-                <Users users={this.state.users} loading={this.state.loading} />
+                <Users users={users} loading={loading} />
               </>
             )}
           />
           <Route path="/about" component={About} />
           <Route path="/user/:login" render={props => (
-            <UserDetails {...props} getUser= {this.getUser} user={this.state.user} loading= {this.state.loading} getUserRepos={this.getUserRepos} repos={this.state.repos} />
+            <UserDetails {...props} getUser= {getUser} user={user} loading= {loading} getUserRepos={getUserRepos} repos={repos} />
           )} />
         </Switch>
       </BrowserRouter>
     )
-  }
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("node:path/win32");
 
 module.exports = {
-    entry: "./src/examples/note_app/index.js",
+    entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
